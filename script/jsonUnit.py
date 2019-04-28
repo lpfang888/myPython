d8daeb39517f40ce4374a7099558b7ce297fcef7
fanglp <fanglp@yiche.com>
2019-04-28 15:19:10 +0800

    添加json实例

--- script/jsonUnit.py
+++ script/jsonUnit.py
@@ -1,50 +1,19 @@
 #!/usr/bin/python3
-# coding=utf-8
-import urllib.request
 
+import json
 
-def get_html(url):
-    page = urllib.request.urlopen(url)
-    html = page.read()
-    html = html.decode('gb2312')
-    return html
+# Python 字典类型转换为 JSON 对象
+data1 = {
+    'no': 1,
+    'name': 'Runoob',
+    'url': 'http://www.runoob.com'
+}
 
+json_str = json.dumps(data1)
+print("Python 原始数据：", repr(data1))
+print("JSON 对象：", json_str)
 
-def view_stock(stock_code):
-    url = "http://hq.sinajs.cn/list={}".format(stock_code)
-    resp = get_html(url)
-    str_array = resp.split(",")
-    for number in range(len(str_array)):
-        if number == 0:
-            print(str_array[number][21:50])
-        elif number == 1:
-            print("今日开盘价:", str_array[number])
-        elif number == 2:
-            print("昨日收盘价:", str_array[number])
-        elif number == 3:
-            print('[当前价格]:\033[1;35m %s \033[0m' % str_array[number])
-        elif number == 4:
-            print("今日最高价:", str_array[number])
-        elif number == 5:
-            print("今日最低价:", str_array[number])
-    a = float(str_array[2])
-    b = float(str_array[3])
-    c = ((b - a) / a) * 100
-    print("[今日跌涨]:%.2f" % c, "%")
-    return str_array[3]
-
-
-stock_list = ["sz000063", "sh603305", "sh600031", "sz000725", "sz000100"]
-
-buy_price = [29.860, 0, 12.430, 4.000, 3.940]
-
-for i in range(len(stock_list)):
-    print("=========================================================")
-    current_price = view_stock(stock_list[i])
-    print("[购买价格]:\033[1;32m %s \033[0m" % buy_price[i])
-
-    difference_price = float(current_price) - buy_price[i]
-    if difference_price > 0:
-        print("[差异价格]:\033[1;35m %.2f \033[0m" % difference_price)
-    else:
-        print("[差异价格]:\033[1;32m %.2f \033[0m" % difference_price)
+# 将 JSON 对象转换为 Python 字典
+data2 = json.loads(json_str)
+print("data2['name']: ", data2['name'])
+print("data2['url']: ", data2['url'])
